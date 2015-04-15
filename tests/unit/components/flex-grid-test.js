ec267aec828d0694db453ab1d9ef0a2f29460b24
seymac <seymaccakmak@gmail.com>
2015-04-15 14:44:01 +0300

    adding failing tests to add dataSource property to flex-grid

--- tests/unit/components/flex-grid-test.js
+++ tests/unit/components/flex-grid-test.js
@@ -5,8 +5,11 @@ import Ember from 'ember';
 
 var App;
 
+var gridData = [ { id: 1, title: 'A'}, { id: 2, title: 'B'}];
+
 moduleForComponent('flex-grid', 'FlexGridComponent', {
   beforeEach: function() {
+    this.subject().set('dataSource', gridData);
     App = startApp();
   },
   afterEach: function() {
@@ -20,7 +23,17 @@ test('is a table tag', function(assert) {
   assert.equal(tagName, expectedTagName);
 
   this.subject().willDestroyElement();
-}
+});
 
 
-);
+test('row number is equal to length of dataSource', function(assert) {
+  var rowNum = $('table >tbody >tr').length;
+
+  assert.equal(rowNum, gridData.length);
+
+  this.subject().willDestroyElement();
+});
+
+
+
+
