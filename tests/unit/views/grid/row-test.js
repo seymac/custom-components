9e05a4e13aa806cabdcc780dc3a663d482cedda4
seymac <seymaccakmak@gmail.com>
2015-04-16 17:00:36 +0300

    adding failing test for row content

--- tests/unit/views/grid/row-test.js
+++ tests/unit/views/grid/row-test.js
@@ -7,6 +7,8 @@ import Ember from 'ember';
 
 var App;
 
+var content = { id: 1, title: 'A', description: 'description-a'};
+
 moduleFor('view:grid/row', 'RowView', {
   beforeEach: function() {
     App = startApp();
@@ -20,6 +22,14 @@ test('is a row tag', function(assert) {
   var tagName = this.subject().tagName;
   var expectedTagName = 'tr';
   assert.equal(tagName, expectedTagName);
+
+  this.subject().willDestroyElement();
+})
+
+
+test('get content from outside', function(assert) {
+  var tagName = this.subject().get('content');
+  assert.equal(tagName, content);
 
   this.subject().willDestroyElement();
 });
