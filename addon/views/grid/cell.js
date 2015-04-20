84dad7d7008852b24862059dad03819ae7cf82ef
seymac <seymaccakmak@gmail.com>
2015-04-20 13:32:43 +0300

    fixing failing test for grid cell tag

---
+++ addon/views/grid/cell.js
@@ -0,0 +1,8 @@
+/**
+ * Created by scakmak on 20/04/2015.
+ */
+import Ember from 'ember';
+
+export default Ember.View.extend({
+  tagName: 'td'
+});
---
+++ app/views/grid/cell.js
@@ -0,0 +1,9 @@
+/**
+ * Created by scakmak on 20/04/2015.
+ */
+
+import Ember from 'ember';
+import Cell from 'custom-components/views/grid/cell';
+
+export default Cell;
+
