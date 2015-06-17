ed9c5df4061b4b7b8256f18bf6ec2b0a759103db
seymac <seymaccakmak@gmail.com>
2015-06-17 18:47:39 +0300

    adding pagination component to grid

--- addon/components/flex-grid.js
+++ addon/components/flex-grid.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'table',
   dataSource: Ember.A([]),
-  columnProperties: Ember.A([])
+  columnProperties: Ember.A([]),
+  pageLength: 5,
+  currentPage: 1,
+
+  totalPageNumber: Ember.computed('dataSource', 'pageLength', function(){
+  	var dataSource = this.get('dataSource');
+  	var pageLength = this.get('pageLength');
+
+  	return Math.ceil(dataSource.length / pageLength);
+  })
 });
